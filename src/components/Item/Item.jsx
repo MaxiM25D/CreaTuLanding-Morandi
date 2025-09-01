e46663cfc037724578b8bcba406b2ed2e0672f17
Maximiliano Morandi <maximorandi19@gmail.com>
2025-09-01 12:15:29 -0300

    Ajustes en inicio.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,7 +3,9 @@ import "./Item.css";
 function Item({ producto }) {
   return (
     <div className="producto-card">
+      <Link to={`/producto/${producto.id}`}>
       <img src={producto.img} alt={producto.nombre} className="producto-img" />
+      </Link>
       <h3 className="producto-nombre">{producto.nombre}</h3>
       <p className="producto-precio">${producto.precio}</p>
       <Link to={`/producto/${producto.id}`} className="btn-detalle">
--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -3,7 +3,9 @@ import productos from "../../data/productos";
 import "./Inicio.css";
 
 function Inicio({ greeting }) {
-  const destacado = productos.find((prod) => prod.id === "7");
+  const destacados = productos.filter(
+    (prod) => prod.id === "7" || prod.id === "1"
+  );
 
   return (
     <section className="inicio">
@@ -12,14 +14,27 @@ function Inicio({ greeting }) {
         Bienvenido a <span className="brand">SM Glamour 🌟</span>, tu tienda de accesorios exclusivos.
       </p>
 
-      <div className="inicio-banner">
-        <Link to={`/producto/${destacado.id}`}>
-          <img
-            className="banner-img"
-            src={destacado.img}
-            alt={destacado.nombre}
-          />
-        </Link>
+      <div className="inicio-destacados">
+        {destacados.length > 0 ? (
+          destacados.map((prod) => (
+            <div key={prod.id} className="inicio-banner">
+              <Link to={`/producto/${prod.id}`}>
+                <img
+                  className="banner-img"
+                  src={prod.img}
+                  alt={prod.nombre}
+                />
+              </Link>
+              <h3 className="banner-title">{prod.nombre}</h3>
+              <p className="producto-precio">${prod.precio}</p>
+              <Link to={`/producto/${prod.id}`} className="btn-detalle">
+                Ver Detalle...
+              </Link>
+            </div>
+          ))
+        ) : (
+          <p>No se encontraron productos destacados...</p>
+        )}
       </div>
     </section>
   );
